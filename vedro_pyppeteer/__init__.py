073039cce8e1859397ccb6e069c38095499e5f02
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-17 22:55:34 +0300

    add config

--- vedro_pyppeteer/__init__.py
+++ vedro_pyppeteer/__init__.py
@@ -1,4 +1,4 @@
-from ._vedro_pyppeteer import PyppeteerPlugin, opened_browser, opened_browser_page
+from ._vedro_pyppeteer import PyppeteerPlugin, opened_browser, opened_browser_page, Pyppeteer
 
 __version__ = "0.4.0"
-__all__ = ("PyppeteerPlugin", "opened_browser", "opened_browser_page",)
+__all__ = ("Pyppeteer", "PyppeteerPlugin", "opened_browser", "opened_browser_page",)
