471f4d1434b6157d34591c8bf468f655df624e38
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-09-04 16:52:09 +0300

    bump version → 1.1.0

--- vedro_pyppeteer/__init__.py
+++ vedro_pyppeteer/__init__.py
@@ -1,4 +1,4 @@
 from ._vedro_pyppeteer import Pyppeteer, PyppeteerPlugin, opened_browser, opened_browser_page
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __all__ = ("Pyppeteer", "PyppeteerPlugin", "opened_browser", "opened_browser_page",)
